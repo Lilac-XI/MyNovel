ce353619b43338a5aa8ea0ab03c566175c214e82
Lilax-XI <xi.lilac@gmail.com>
2024-07-08 00:44:33 +0900

    index漏れ

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="post-content">
+            <?php the_excerpt(); ?>
+        </div>
+    <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
